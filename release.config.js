5f3985ab3bf9fb020044a2b9fbdf44f4730354df
trungpt12 <trungpt1@fpt.com>
2023-07-06 11:24:55 +0700

    add github flow

--- release.config.js
+++ release.config.js
@@ -1,12 +1,11 @@
 module.exports = {
 	branches: "master",
-	repositoryUrl: "https://github.com/alialaa/react-app",
+	repositoryUrl: "https://github.com/trungpt12/silabs-test-github",
 	plugins: [
 		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
 		["@semantic-release/github", {
 			assets: [
 				{ path: "build.zip", label: "Build" },
-				{ path: "coverage.zip", label: "Coverage" }
 			]
 		}]
 		]
